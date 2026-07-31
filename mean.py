6b775c3fa5cf1a3b18174db870766a39902e48f7
TheSonu281 <sonusaji116@gmail.com>
2026-07-31 08:23:23 +0530

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 31 08:20:14 2026
+
+@author: USER
+"""
+
+data = [1,2,3,4,5]
+sum = 0
+for i in data:
+    sum = sum+i
+l = len(data)
+mean = sum / l
+print("mean: ",mean)
